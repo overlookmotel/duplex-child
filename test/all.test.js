bd8a0643512985d2d2b305cff455626158a2a507
overlookmotel <theoverlookmotel@gmail.com>
2018-04-27 17:54:40 +0100

    WIP1

--- test/all.test.js
+++ test/all.test.js
@@ -8,18 +8,45 @@
 // Modules
 const chai = require('chai'),
 	{expect} = chai,
-	childDuplex = require('../lib/');
+	fs = require('fs'),
+	pathJoin = require('path').join,
+	DuplexChild = require('../lib/');
 
 // Init
 chai.config.includeStack = true;
 
 // Tests
+const pathIn = pathJoin(__dirname, 'files'),
+	pathOut = pathJoin(__dirname, 'filesOut');
 
 /* jshint expr: true */
 /* global describe, it */
 
 describe('Tests', function() {
-	it.skip('all', function() {
-		expect(childDuplex).to.be.ok;
+	it('check', function(cb) {
+		const filename = '100KB.txt';
+
+		console.log(`test with file: ${filename}`);
+
+		console.log('creating DuplexChild');
+		const cat = new DuplexChild();
+		console.log('spawning');
+		cat.spawn('cat');
+		console.log('spawned');
+
+		const input = fs.createReadStream(pathJoin(pathIn, filename)),
+			output = fs.createWriteStream(pathJoin(pathOut, filename));
+
+		console.log('piping input');
+		input.pipe(cat);
+		console.log('piped input');
+
+		console.log('piping output');
+		cat.pipe(output);
+		console.log('piped output');
+
+		expect(true).to.be.true;
+
+		output.on('close', cb);
 	});
 });
